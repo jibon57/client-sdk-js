1c5b405c4cd3b7dd6b649c624620d057c06edf70
Jibon <jiboncosta57@gmail.com>
2023-03-21 13:53:17 +0100

    temporary disable

---
+++ dist/src/room/track/RemoteVideoTrack.d.ts
@@ -0,0 +1,61 @@
+import RemoteTrack from './RemoteTrack';
+import type { AdaptiveStreamSettings } from './types';
+export default class RemoteVideoTrack extends RemoteTrack {
+    private prevStats?;
+    private elementInfos;
+    private adaptiveStreamSettings?;
+    private lastVisible?;
+    private lastDimensions?;
+    private hasUsedAttach;
+    constructor(mediaTrack: MediaStreamTrack, sid: string, receiver?: RTCRtpReceiver, adaptiveStreamSettings?: AdaptiveStreamSettings);
+    get isAdaptiveStream(): boolean;
+    get mediaStreamTrack(): MediaStreamTrack;
+    /** @internal */
+    setMuted(muted: boolean): void;
+    attach(): HTMLMediaElement;
+    attach(element: HTMLMediaElement): HTMLMediaElement;
+    /**
+     * Observe an ElementInfo for changes when adaptive streaming.
+     * @param elementInfo
+     * @internal
+     */
+    observeElementInfo(elementInfo: ElementInfo): void;
+    /**
+     * Stop observing an ElementInfo for changes.
+     * @param elementInfo
+     * @internal
+     */
+    stopObservingElementInfo(elementInfo: ElementInfo): void;
+    detach(): HTMLMediaElement[];
+    detach(element: HTMLMediaElement): HTMLMediaElement;
+    /** @internal */
+    getDecoderImplementation(): string | undefined;
+    protected monitorReceiver: () => Promise<void>;
+    private getReceiverStats;
+    /**
+     * Start observing an Element for changes.
+     * @param element
+     */
+    startObservingElement(element: HTMLMediaElement): void;
+    /**
+     * Stop observing an Element for changes.
+     * @param element
+     */
+    stopObservingElement(element: HTMLMediaElement): void;
+    protected handleAppVisibilityChanged(): Promise<void>;
+    private readonly debouncedHandleResize;
+    private updateVisibility;
+    private updateDimensions;
+}
+export interface ElementInfo {
+    element: object;
+    width(): number;
+    height(): number;
+    visible: boolean;
+    visibilityChangedAt: number | undefined;
+    handleResize?: () => void;
+    handleVisibilityChanged?: () => void;
+    observe(): void;
+    stopObserving(): void;
+}
+//# sourceMappingURL=RemoteVideoTrack.d.ts.map
---
+++ dist/ts4.2/src/room/types.d.ts
@@ -0,0 +1,21 @@
+import type RemoteParticipant from './participant/RemoteParticipant';
+export type SimulationOptions = {
+    publish?: {
+        audio?: boolean;
+        video?: boolean;
+        useRealTracks?: boolean;
+    };
+    participants?: {
+        count?: number;
+        aspectRatios?: Array<number>;
+        audio?: boolean;
+        video?: boolean;
+    };
+};
+export type DataPublishOptions = {
+    /** the participants who will receive the message, will be sent to every one if empty */
+    destination?: RemoteParticipant[] | string[];
+    /** the topic under which the message gets published */
+    topic?: string;
+};
+//# sourceMappingURL=types.d.ts.map
